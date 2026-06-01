2344a6984132934b34e00cad46406c7347a4fec6
nalin <nalinikantbastia@gmail.com>
2026-06-01 23:55:13 +0530

    URL patterns in Django

--- ch8/ch8/urls.py
+++ ch8/ch8/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from app1.views import learn_django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dj/', learn_django, name='learn_django'),
+    path('py/', learn_django, name='learn_django'),
 ]
