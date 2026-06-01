7a2c635d07d798450532a1571b415e1a059e0997
nalin <nalinikantbastia@gmail.com>
2026-06-01 23:03:12 +0530

    views in MVT architecture

--- CH5/app1/views.py
+++ CH5/app1/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def myfunction(request):
-    return HttpResponse('Hello Django')
 
 
-def learn_math(request):
-    a = 10+20
-    return HttpResponse(a)
+
+
+
+
+
+
+# views is the file where the business lofic is written.
+
+# so this is the example of the views written without using any AI.
+# def myfunction(request):
+#     return HttpResponse('Hello Django')
+
+
+# def learn_math(request):
+#     a = 10+20
+#     return HttpResponse(a)
