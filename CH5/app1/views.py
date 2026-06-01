8d415e7ae661c9cb96471cd35c3db3e02b1c8482
nalin <nalinikantbastia@gmail.com>
2026-06-01 23:16:38 +0530

    views modification

--- CH5/app1/views.py
+++ CH5/app1/views.py
@@ -5,13 +5,7 @@ from django.http import HttpResponse
 
 
 
-
-
-
-
-
-# views is the file where the business lofic is written.
-
+# views is the file where the business logic is written.
 # so this is the example of the views written without using any AI.
 # def myfunction(request):
 #     return HttpResponse('Hello Django')
@@ -20,3 +14,9 @@ from django.http import HttpResponse
 # def learn_math(request):
 #     a = 10+20
 #     return HttpResponse(a)
+
+def home(request):
+    return HttpResponse('Home Page')
+
+def myapp1(request):
+    return HttpResponse('My  App Page')
--- CH5/ch5/urls.py
+++ CH5/ch5/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import views
+from app1 import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myfun/', views.myfunction, name='myfunction'),
-    path('Learnmath/', views.learn_math, name='learn_math')
+    # path('myfun/', views.myfunction, name='myfunction'),
+    # path('Learnmath/', views.learn_math, name='learn_math')
+    path('', views.home, name='home'),
+    path('myapp1/', views.myapp1, name='myapp1'),
 ]
