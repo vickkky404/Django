b44303d84403f07848a96335f02189e5feacd2e2
nalin <nalinikantbastia@gmail.com>
2026-06-01 21:08:40 +0530

    .

--- CH5/app1/views.py
+++ CH5/app1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def myfunction(request):
+    return HttpResponse('Hello Django')
+
+
+def learn_math(request):
+    a = 10+20
+    return HttpResponse(a)
--- CH5/ch5/urls.py
+++ CH5/ch5/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('myfun/', views.myfunction, name='myfunction'),
+    path('Learnmath/', views.learn_math, name='learn_math')
 ]
